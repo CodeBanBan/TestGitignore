35c19d5e2e77f8bfa11c981cb8a55bac43e6af82
codebanban <codebanban@gmail.com>
2013-01-16 14:02:10 +0700

    New File

---
+++ TestGitignore/TestGitignore/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestGitignore
+//
+//  Created by Code BanBan on 1/16/56 BE.
+//  Copyright (c) 2556 Code BanBan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestGitignore/TestGitignore/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestGitignore
+//
+//  Created by Code BanBan on 1/16/56 BE.
+//  Copyright (c) 2556 Code BanBan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
